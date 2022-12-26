d981e151c6588ff325dfec165904f2c3eeb834c8
timli12 <117094568+timli12@users.noreply.github.com>
2022-12-26 23:10:46 +0800

    update

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ var options2 = {
   maxAge: '1m',
   redirect: false
 }
+var options3 = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['data.html'],
+  maxAge: '1m',
+  redirect: false
+}
 mongoose.set('strictQuery', true);
 const connectDB = async () => {
     try {
@@ -35,6 +43,7 @@ const connectDB = async () => {
   }
   app.use("/", express.static('public', options));
   app.use("/preorder", express.static('public', options2));
+  app.use("/data", express.static('public', options3));
   app.use(express.json());
   //Routes go here
   app.use("/",formRouter);
