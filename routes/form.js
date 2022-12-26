a1e8c71a748efb6e7910ccaa00e6dc59b7c9c402
timli12 <117094568+timli12@users.noreply.github.com>
2022-12-27 00:51:36 +0800

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -34,7 +34,8 @@ router.post("/app", async (req, res) => {
         // 使用.save()將資料存進資料庫
         await form.save();
         // 回傳status:201代表新增成功 並回傳新增的資料
-        res.status(201).json("success");
+        res.json("預約成功");
+        res.redirect('/preorder');
     } catch (err) {
         // 錯誤訊息發生回傳400 代表使用者傳入錯誤的資訊
         res.status(400).json({ message: err.message })
