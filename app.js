b1ec2b27f87cf163629a8a72d4788ad1252308df
timli12 <117094568+timli12@users.noreply.github.com>
2022-12-26 21:56:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require("./config/config");
 const app = express()
+const config = require("./config/config");
+const formRouter = require("./routes/form");
+const port = config.app.port;
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['homepage.html'],
+  maxAge: '1m',
+  redirect: false
+}
 mongoose.set('strictQuery', true);
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(config.db.url);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
+    try {
+      const conn = await mongoose.connect(config.db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+        });
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
   }
-}
-
-//Routes go here
-app.all('*', (req,res) => {
-    res.json({"every thing":"is awesome"})
-})
-
-//Connect to the database before listening
-connectDB().then(() => {
-    app.listen(config.app.port, () => {
-        console.log("listening for requests");
-    })
-})
+  app.use(express.static('public', options));
+  //Routes go here
+  app.use("/",formRouter);
+  //Connect to the database before listening
+  connectDB().then(() => {
+      app.listen(port, () => {
+          console.log("listening for requests");
+      })
+  })
