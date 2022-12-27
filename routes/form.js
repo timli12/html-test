5cf2e83f7c7588ad2ee38999ca815a3aa2b9d1fc
timli12 <117094568+timli12@users.noreply.github.com>
2022-12-27 14:19:51 +0800

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -34,7 +34,7 @@ router.post("/app", async (req, res) => {
     try {
         // 使用.save()將資料存進資料庫
         await form.save();
-        res.redirect("/");
+        res.render('red.html');
     } catch (err) {
         // 錯誤訊息發生回傳400 代表使用者傳入錯誤的資訊
         res.status(400).json({ message: err.message, name: fullname });
