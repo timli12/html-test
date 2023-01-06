20de542e1e7e58ce354bdf1fb512519bfa372afe
timli12 <117094568+timli12@users.noreply.github.com>
2023-01-06 18:11:50 +0800

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -155,7 +155,6 @@ async function ensureAuthenticated(req, res, next){
     if(req.isAuthenticated()){
       return next();
     } else {
-      req.flash('error_msg', 'you are not logged in')
       res.redirect('/users/signin')
     }
   }
